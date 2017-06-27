da61b31358b7b9eb54442639fdbf52791e92304c
felixlinker <linkerfelix@gmail.com>
2017-06-27 18:26:35 +0200

    pretty_print.write now auto-converst to string

--- pretty_print.py
+++ pretty_print.py
@@ -7,12 +7,12 @@ def write(matrix):
     tabs = [0 for i in range(len(matrix[0]))]
     for j in range(len(matrix[0])):
         for i in range(len(matrix)):
-            tabs[j] =  max(tabs[j], ceil((len(matrix[i][j]) + 1) / __TAB_WIDTH))
+            tabs[j] =  max(tabs[j], ceil((len(str(matrix[i][j])) + 1) / __TAB_WIDTH))
 
     for line in matrix:
         line_str = ''
         for i in range(len(line)):
-            cell = line[i]
+            cell = str(line[i])
             line_str += cell
             line_str += '\t' * ceil(
                 ((tabs[i] * __TAB_WIDTH) - len(cell)) / __TAB_WIDTH
