bb00434d8a0991d02ff6ad9b2391f4edb0674fa4
felixlinker <felixlinker.github@outlook.com>
2017-07-01 10:30:22 +0200

    Improved parameters to latest_data.bat

--- latest_data.bat
+++ latest_data.bat
@@ -1,3 +1,3 @@
 SET market=C:\Users\felix\OneDrive\Elite Dangerous\EDMarketConnector
-FOR /F "delims=" %%I IN ('DIR "%market%\%~1.%~2*" /B /O:-D') DO python . "%market%\%%I" -sd %3 -bd %3 -e & GOTO STOP
+FOR /F "delims=" %%I IN ('DIR "%market%\%~1.%~2*" /B /O:-D') DO python . "%market%\%%I" %~3 -f %4 -t %5 & GOTO STOP
 :STOP
